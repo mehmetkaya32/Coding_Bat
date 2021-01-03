f89bf3a7b8b31a1b90dccbb594b3178303c2d260
MehmetKaya <mkaya.us@gmail.com>
2021-01-03 16:49:06 -0500

    MyChanges

---
+++ src/com/Array1/MakeEnds.java
@@ -0,0 +1,18 @@
+package com.Array1;
+
+import java.util.Arrays;
+
+public class MakeEnds {
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(makeEnds(new int[]{1,4,5,9})));
+
+
+    }
+    public static int[] makeEnds(int[] nums) {
+
+        return new int[] {nums [0],nums[nums.length-1]};
+
+    }
+}
